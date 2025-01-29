08a9e88b50d522ec848637aed0b2c1d57be48311
swathi-parvathaneni <134302064+swathi-veytel@users.noreply.github.com>
2025-01-29 20:51:53 +0000

    opencv test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 import streamlit as st
-import cv2 
+import cv2
+
+
 
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+# Check OpenCV version
+st.write("OpenCV version:", cv2.__version__)
